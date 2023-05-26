6d25e3a58ee0e70ed698e96a0ef74b7e17cbbd10
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-05-26 17:38:14 -0300

    feat: update user-repository interface

--- src/modules/users/repositories/users-repository.ts
+++ src/modules/users/repositories/users-repository.ts
@@ -1,7 +1,20 @@
-import { ICreateUserDTO } from '../dtos/create-user';
+import { IPagination } from 'src/shared/utils/pagination';
 import { User } from '../entities/user.entity';
+import {
+  ICreateUserDTO,
+  IFindManyDTO,
+  IUpdateUserDTO,
+  IUpdateUserPasswordDTO,
+} from '../dtos';
 
 export abstract class IUsersRepository {
   abstract create(data: ICreateUserDTO): Promise<User>;
+  abstract save(updateUserDTO: IUpdateUserDTO): Promise<User>;
+  abstract savePassword(
+    updateUserPasswordDTO: IUpdateUserPasswordDTO,
+  ): Promise<User>;
   abstract findByEmail(email: string): Promise<User>;
+  abstract findMany(options: IFindManyDTO): Promise<IPagination<User>>;
+  abstract findById(userId: string): Promise<User | null>;
+  abstract delete(userId: string): Promise<void>;
 }
