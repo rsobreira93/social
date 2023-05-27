2667aa41046f6cd3d8d362dbae9fd2d5c11e0020
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-05-27 07:51:24 -0300

    feat: add new methods to repository

--- src/modules/users/infra/prisma/repositories/user-repository.ts
+++ src/modules/users/infra/prisma/repositories/user-repository.ts
@@ -1,12 +1,21 @@
+import {
+  IFindManyDTO,
+  IUpdateUserAvatarDTO,
+  IUpdateUserDTO,
+  IUpdateUserPasswordDTO,
+} from '@modules/users/dtos';
+import { ICreateUserDTO } from '@modules/users/dtos/create-user';
+import { User } from '@modules/users/entities/user.entity';
+import { IUsersRepository } from '@modules/users/repositories/users-repository';
 import { Injectable } from '@nestjs/common';
-import { ICreateUserDTO } from 'src/modules/users/dtos/create-user';
-import { User } from 'src/modules/users/entities/user.entity';
-import { IUsersRepository } from 'src/modules/users/repositories/users-repository';
-import { PrismaService } from '../../../../../shared/database/prisma/prisma.service';
+import { PrismaService } from '@shared/database/prisma/prisma.service';
+import { formatPagination } from '@shared/utils/format-pagination';
+import { IPagination } from '@shared/utils/pagination';
 
 @Injectable()
 export class UsersRepository implements IUsersRepository {
   constructor(private readonly prisma: PrismaService) {}
+
   async create(data: ICreateUserDTO): Promise<User> {
     const user = await this.prisma.user.create({
       data,
@@ -19,5 +28,95 @@ export class UsersRepository implements IUsersRepository {
     const user = await this.prisma.user.findFirst({ where: { email } });
 
     return user;
+  }
+
+  async save({
+    userId,
+    email,
+    name,
+    birthDate,
+  }: IUpdateUserDTO): Promise<User> {
+    const userUpdated = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        name,
+        email,
+        birthDate,
+      },
+    });
+
+    return userUpdated;
+  }
+
+  async savePassword({
+    userId,
+    password,
+  }: IUpdateUserPasswordDTO): Promise<User> {
+    const userUpdatedPassword = await this.prisma.user.update({
+      where: { id: userId },
+      data: { password },
+    });
+
+    return userUpdatedPassword;
+  }
+
+  public async saveAvatarUrl({
+    userId,
+    avatarUrl,
+  }: IUpdateUserAvatarDTO): Promise<User> {
+    const userUpdatedAvatarUrl = await this.prisma.user.update({
+      where: { id: userId },
+      data: { avatarUrl },
+    });
+
+    return userUpdatedAvatarUrl;
+  }
+
+  public async findMany(options: IFindManyDTO): Promise<IPagination<User>> {
+    const { emailContains, nameContains, page, perPage } = options;
+
+    const skip = (page - 1) * perPage;
+
+    const [users, count] = await this.prisma.$transaction([
+      this.prisma.user.findMany({
+        skip,
+        take: perPage,
+        where: {
+          email: { contains: emailContains, mode: 'insensitive' },
+          name: { contains: nameContains, mode: 'insensitive' },
+        },
+        orderBy: { createdAt: 'desc' },
+      }),
+      this.prisma.user.count({
+        where: {
+          email: { contains: emailContains, mode: 'insensitive' },
+          name: { contains: nameContains, mode: 'insensitive' },
+        },
+      }),
+    ]);
+
+    const usersPaginate = formatPagination({
+      page,
+      perPage,
+      skip,
+      count,
+      data: users,
+    });
+
+    return usersPaginate;
+  }
+
+  async findById(userId: string): Promise<User> {
+    const user = await this.prisma.user.findFirst({
+      where: { id: userId },
+    });
+
+    return user;
+  }
+
+  async delete(userId: string): Promise<void> {
+    await this.prisma.user.delete({
+      where: { id: userId },
+    });
   }
 }
--- src/modules/users/repositories/users-repository.ts
+++ src/modules/users/repositories/users-repository.ts
@@ -1,17 +1,21 @@
-import { IPagination } from 'src/shared/utils/pagination';
-import { User } from '../entities/user.entity';
+import { IPagination } from '@shared/utils/pagination';
 import {
   ICreateUserDTO,
   IFindManyDTO,
+  IUpdateUserAvatarDTO,
   IUpdateUserDTO,
   IUpdateUserPasswordDTO,
 } from '../dtos';
+import { User } from '../entities/user.entity';
 
 export abstract class IUsersRepository {
   abstract create(data: ICreateUserDTO): Promise<User>;
   abstract save(updateUserDTO: IUpdateUserDTO): Promise<User>;
   abstract savePassword(
     updateUserPasswordDTO: IUpdateUserPasswordDTO,
+  ): Promise<User>;
+  abstract saveAvatarUrl(
+    updateUserAvatarDTO: IUpdateUserAvatarDTO,
   ): Promise<User>;
   abstract findByEmail(email: string): Promise<User>;
   abstract findMany(options: IFindManyDTO): Promise<IPagination<User>>;
