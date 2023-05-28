f605f3570c6e5387d669bf12a3ca7ce93bb8341e
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-05-28 12:05:24 -0300

    feat: add post module to app module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PostModule } from '@modules/posts/post.module';
 import { Module } from '@nestjs/common';
 import { LoggerModule } from '@shared/loggers/logger.module';
 import { AwsConfigModule } from '@shared/s3/uploaderS3.module';
@@ -6,7 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [UserModule, LoggerModule, AwsConfigModule],
+  imports: [UserModule, PostModule, LoggerModule, AwsConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
