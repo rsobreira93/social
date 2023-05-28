b8d8211fafba1c715dc880b6d6ba0ecb587d463b
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-05-28 12:06:14 -0300

    feat: add post repository to prisma module

--- src/shared/database/prisma/prisma.module.ts
+++ src/shared/database/prisma/prisma.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { PostsRepository } from '@modules/posts/infra/prisma/repositories/pots-repository';
+import { IPostsRepository } from '@modules/posts/repositories/posts-repository';
 import { UsersRepository } from '@modules/users/infra/prisma/repositories/user-repository';
 import { IUsersRepository } from '@modules/users/repositories/users-repository';
+import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
@@ -10,7 +12,11 @@ import { PrismaService } from './prisma.service';
       provide: IUsersRepository,
       useClass: UsersRepository,
     },
+    {
+      provide: IPostsRepository,
+      useClass: PostsRepository,
+    },
   ],
-  exports: [IUsersRepository],
+  exports: [IUsersRepository, IPostsRepository],
 })
 export class PrismaModule {}
