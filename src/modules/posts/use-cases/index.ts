a9517b430a643dad04eb129fa6aba0dac48a1cba
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-05-28 12:13:45 -0300

    feat: add index to simplify imports on use cases

---
+++ src/modules/posts/use-cases/index.ts
@@ -0,0 +1,5 @@
+export { CreatePost } from './create-post';
+export { DeletePost } from './delete-post';
+export { ListPosts } from './list-posts';
+export { ListPostsByUser } from './list-posts-by-user';
+export { UpdatePost } from './update-post';
