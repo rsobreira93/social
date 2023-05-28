1c53457259279155afe469e2dc6a34ec20e94028
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-05-28 12:13:56 -0300

    feat: add index to simplify imports on dtos

---
+++ src/modules/posts/dtos/index.ts
@@ -0,0 +1,4 @@
+export { ICreatePostDTO } from './create-post';
+export { IFindManyDTO } from './find-many';
+export { IFindPostsByUserDTO } from './find-posts-by-user';
+export { IUpdatePostDTO } from './update-post';
